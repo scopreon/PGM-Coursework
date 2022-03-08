e2e4ef3dea934febc913e55be8a499fd26b25a79
Saul Cooperman <sc21sc@leeds.ac.uk>
2022-03-08 14:55:22 +0000

    Create struct for file Create modules for validating file

---
+++ assignment_1/image.h
@@ -0,0 +1,27 @@
+#ifndef IMAGE_STRUCT
+#define IMAGE_STRUCT
+
+struct image
+{ 
+    char *commentLine;
+    unsigned short *magic_Number;
+    unsigned char magic_number[2];
+	/* the logical width & height	         */
+	/* note: cannot be negative	         */
+	unsigned int width;
+    unsigned int height;
+
+	/* maximum gray value (assumed)	         */
+	/* make it an integer for ease	         */
+	unsigned int maxGray;
+
+	/* pointer to raw image data	         */
+	unsigned char *imageData;
+	
+	/* now start reading in the data         */
+	/* try to open the file for text I/O     */
+	/* in ASCII mode b/c the header is text  */
+	FILE *inputFile;
+};
+
+#endif
